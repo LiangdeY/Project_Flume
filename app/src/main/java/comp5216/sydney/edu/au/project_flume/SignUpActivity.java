4d660a9a015edbf0d343a42cbc64416ec9dd3136
Kim <yldissaier@gmail.com>
2019-10-19 23:24:23 +1100

    firebase

--- app/src/main/java/comp5216/sydney/edu/au/project_flume/SignUpActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/project_flume/SignUpActivity.java
@@ -3,12 +3,9 @@ package comp5216.sydney.edu.au.project_flume;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
-    private FirebaseAuth mFirebaseAuth;
 
 
     @Override
@@ -16,20 +13,9 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_up_button:
-                signIn();
-                break;
-        }
-    }
-    private void signIn() {
 
-    }
 
 
 }
