7b977d84ecec81d7b3f0110fb6f4ca5857a5952e
yldissaier@gmail.com <kieren007>
2019-10-22 14:46:08 +1100

    merged main and login

--- app/src/main/java/comp5216/sydney/edu/au/project_flume/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/project_flume/MainActivity.java
@@ -1,29 +1,42 @@
 package comp5216.sydney.edu.au.project_flume;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Button login, signUp;
+    FirebaseAuth auth;
+    EditText email, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        auth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
 
         login = findViewById(R.id.login_main);
         signUp = findViewById(R.id.signUp_main);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity( new Intent(MainActivity.this, LoginActivity.class) );
+                Login();
             }
         });
         signUp.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +61,33 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
+    private void Login() {
+        String sEmail= email.getText().toString();
+        String sPassword = password.getText().toString();
 
+        if(sEmail.isEmpty() || sPassword.isEmpty()){
+            Toast.makeText(MainActivity.this, "all fields must be filled",
+                    Toast.LENGTH_SHORT).show();
+        }else{
+            auth.signInWithEmailAndPassword(sEmail,sPassword).addOnCompleteListener(
+                    new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Intent i = new Intent(MainActivity.this,
+                                        HomeActivity.class);
+                                i.putExtra("login", "1");
+                                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                                        Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(i);
+                                finish();
+                            }else {
+                                Toast.makeText(MainActivity.this,
+                                        "Authentication Fail!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
 
 }
