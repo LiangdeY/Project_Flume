5438182874c0edaa8b5844eed7bda3abe2b38523
yldissaier@gmail.com <kieren007>
2019-10-24 06:34:47 +1100

    merge

--- app/src/main/java/comp5216/sydney/edu/au/project_flume/Model/MyProgressBar.java
+++ app/src/main/java/comp5216/sydney/edu/au/project_flume/Model/MyProgressBar.java
@@ -1,12 +1,12 @@
 package comp5216.sydney.edu.au.project_flume.Model;
 
 public class MyProgressBar {
-    String setter;
-    String max;
-    String viewer;
-    String progress;
+    private String setter;
+    private String viewer;
+    private String max;
+    private String progress;
 
-    public MyProgressBar(String setter, String max, String viewer, String progress) {
+    public MyProgressBar(String setter, String viewer, String max, String progress) {
         this.setter = setter;
         this.max = max;
         this.viewer = viewer;
@@ -17,23 +17,22 @@ public class MyProgressBar {
     public String getSetter() {
         return setter;
     }
-
     public void setSetter(String setter) {
         this.setter = setter;
-    }
-
-    public String getMax() {
-        return max;
-    }
-
-    public void setMax(String max) {
-        this.max = max;
     }
 
     public String getViewer() { return viewer; }
     public void setViewer(String viewer) {
         this.viewer = viewer;
     }
+
+    public String getMax() {
+        return max;
+    }
+    public void setMax(String max) {
+        this.max = max;
+    }
+
 
     public String getProgress() {
         return progress;
